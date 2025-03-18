44b159dbad11d85e8ca0ccfab32a239477bed6ee
lasysz <laistenorio7@gmail.com>
2025-03-18 09:07:02 -0300

    Classe Aluno

---
+++ DevOpsLais/Models/Aluno.cs
@@ -0,0 +1,13 @@
+﻿namespace DevOpsLais.Models
+{
+    public class Aluno
+    {
+        public Guid Id { get; set; }
+        public String? Nome { get; set; }
+        public String? RGM { get; set; }
+        public String? Professor{ get; set; }
+        public String? Email { get; set; }
+        public DateTime Idade { get; set; }
+
+    }
+}
